221b5d2a49736ed268aa926ed63ebea975fc8169
anh0034 <hayandrew2@gmail.com>
2023-02-05 20:05:09 -0500

    fsf

--- js/code.js
+++ js/code.js
@@ -259,3 +259,11 @@ function nextjacket()
     
     
 }
+function hideAll() {
+    var x = document.getElementsByClassName("btn84");
+      if (x.display === "none") {
+    x.display = "block";
+    } else {
+    x.display = "none";
+      }
+    } 
